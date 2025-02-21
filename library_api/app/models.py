396d8060170d361346403efd0d03368e62beb643
gajawada yashwanth <gajawadayashwanth14@gmail.com>
2025-02-21 13:43:07 +0530

    Added the models.py file

---
+++ library_api/app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.schema import UniqueConstraint
+from .database import Base
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    published_year = Column(Integer, nullable=False)
+    isbn = Column(String, unique=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
